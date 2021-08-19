f1797f143660c25759b9da2c8a54d432e4fcfdbf
Mariii-23 <marianadinis8@hotmail.com>
2021-08-19 23:34:25 +0100

    modify movie add show_vc

--- src/commands/movies.rs
+++ src/commands/movies.rs
@@ -427,10 +427,14 @@ async fn choose_vc(ctx: &Context, msg: &Message) -> CommandResult {
 
     //Ir buscar os filmes ao file
     let movies = file_to_struct(msg);
+    let mut all_titles = String::new();
 
     //Ver que filmes podem ser vistos em função  das pessoas na chamada
     let mut ok_movies: Vec<Movie> = Vec::new();
     for movie_aux in movies {
+        all_titles.push_str(&movie_aux.title);
+        all_titles = all_titles + "\n";
+
         let people = &movie_aux.people;
         if people.iter().all(|item| people_vc.contains(item)) {
             if msg.content.contains("exact") {
@@ -447,7 +451,6 @@ async fn choose_vc(ctx: &Context, msg: &Message) -> CommandResult {
     ok_movies.sort_by(|a, b| b.cmp(a));
 
     //Agora tenho um Vec<Movie> é só fazer show deles (teoricamente let mut pages = Vec::new();
-
 
     let mut pages = Vec::new();
     for movie in ok_movies {
@@ -474,6 +477,18 @@ async fn choose_vc(ctx: &Context, msg: &Message) -> CommandResult {
         });
         pages.push(page);
     }
+
+    let msg1 = msg
+        .channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.field("All movies", all_titles, false);
+
+                e
+            });
+            m
+    });
+    msg1.await.unwrap();
 
     // Creates a new menu.
     let menu = Menu::new(ctx, msg, &pages, pagination::simple_options());
